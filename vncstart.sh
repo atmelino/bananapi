7a0d54f43e2af61cd0391d7632f28b5b9a43e0f3
atmelino <tmberthold@gmail.com>
2015-07-14 19:18:39 -0500

    Create vncstart.sh

---
+++ vncstart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#x11vnc -bg -o %HOME/.x11vnc.log.%VNCDISPLAY -auth /var/run/lightdm/root/:0 -forever
+
+x11vnc -bg -geometry 1024x768 -o %HOME/.x11vnc.log.%VNCDISPLAY -auth /var/run/lightdm/root/:0 -forever
+
+
+
+sudo service lightdm restart
+
+echo press enter
+
+read
