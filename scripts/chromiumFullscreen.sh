8f6771d884a233987b9f6e8eb34d8a0bcf2d2c97
jatnik <jatnik@jat>
2015-07-16 20:12:15 -0500

    reorganize, add index.html

---
+++ scripts/chromiumFullscreen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+
+chromium --start-maximized localhost
+sleep 5
+xdotool key F11
+
+echo press enter
+
+read
+
--- scripts/vncstart.sh
+++ scripts/vncstart.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 
+sudo service lightdm restart
+
+
 #x11vnc -bg -o %HOME/.x11vnc.log.%VNCDISPLAY -auth /var/run/lightdm/root/:0 -forever
 
 x11vnc -bg -geometry 1024x768 -o %HOME/.x11vnc.log.%VNCDISPLAY -auth /var/run/lightdm/root/:0 -forever
 
 
 
-sudo service lightdm restart
+#sudo service lightdm restart
 
 echo press enter
 
 read
+
+
