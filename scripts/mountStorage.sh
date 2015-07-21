3f50a0abef4564ebbc6bdab8d85975f41438cf80
bananapi <bananapi@myself>
2015-07-22 05:38:10 +0800

    SD card mount script

---
+++ scripts/mountStorage.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+
+sudo mkdir /mnt/SDpart1
+sudo mount  /dev/mmcblk0p1  /mnt/SDpart1
+sudo mkdir /mnt/SDpart2
+sudo mount  /dev/mmcblk0p2  /mnt/SDpart2
+
+
+echo press enter
+
+read
