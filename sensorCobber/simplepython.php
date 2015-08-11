96a84b5387183c2acf1608301366fe476fdb98ba
jatnik <jatnik@jat>
2015-08-10 20:24:19 -0500

    calling python from PHP

---
+++ sensorCobber/simplepython.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "call python test <br>";
+
+
+$python = `python hello.py`;
+//$python = `python simpletest.py`;
+//$python = `simpletest.sh`;
+
+echo $python;
+echo "<br>";
+
+
+echo "call python end";
+
+ 
+
+
+
+?>
+
+
+
