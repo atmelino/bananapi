911666bcf81ad189f675861eb46baefd87fed6ec
user <user@asu2>
2016-01-06 13:08:48 -0600

    php call without parameters for command line testing

---
+++ projects/servo/servoFunctions_noParam.php
@@ -0,0 +1,14 @@
+<?php
+
+
+print "run python program\n";
+//$cmd = "/usr/bin/python ./Servo_pos300.py 2>&1";
+$cmd = "/usr/bin/python ./Servo_pos300.py";
+$handle = popen($cmd, 'r');
+$res = fread($handle, 8192);
+pclose($handle);
+
+
+
+
+?>
