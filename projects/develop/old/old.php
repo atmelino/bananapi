bc539b348b65739a3aa7f547006b0c4cd071b4e4
user <user@asu2>
2016-01-07 00:25:43 -0600

    try only popen

---
+++ projects/develop/old/old.php
@@ -0,0 +1,30 @@
+$descriptorspec = array(
+		0 => array("pipe","r"),
+		1 => array("pipe","w"),
+		2 => array("file","./error.log","a")
+) ;
+
+echo "proc_open() test <br>";
+
+// define current working directory where files would be stored
+$cwd = './' ;
+
+// open process reprint.pl and pass it an argument
+$process = proc_open('python ./hello.py ' . $argv1, $descriptorspec, $pipes, $cwd) ;
+echo "variable process ";
+echo $process;
+
+echo "<br>";
+
+//$argv1 doing nothing here its just for the example
+if (is_resource($process)) {// print pipe output
+	echo stream_get_contents($pipes1) ;// close pipe
+	fclose($pipes1) ;
+
+	// close process
+	proc_close($process) ;
+
+}
+
+echo "proc_open() end";
+
--- projects/servo/servoFunctions.php
+++ projects/servo/servoFunctions.php
@@ -1,36 +1,6 @@
 <?php
 
 
-$descriptorspec = array(
-		0 => array("pipe","r"),
-		1 => array("pipe","w"),
-		2 => array("file","./error.log","a")
-) ;
-
-echo "proc_open() test <br>";
-
-// define current working directory where files would be stored
-$cwd = './' ;
-
-// open process reprint.pl and pass it an argument
-$process = proc_open('python ./hello.py ' . $argv1, $descriptorspec, $pipes, $cwd) ;
-echo "variable process ";
-echo $process;
-
-echo "<br>";
-
-//$argv1 doing nothing here its just for the example
-if (is_resource($process)) {// print pipe output
-	echo stream_get_contents($pipes1) ;// close pipe
-	fclose($pipes1) ;
-
-	// close process
-	proc_close($process) ;
-
-}
-
-echo "proc_open() end";
-
 
 
 
@@ -39,7 +9,8 @@ if ( isset($_GET['angle']))
 {
 	$angle = $_GET['angle'];
 
-
+	echo "angle: "+$angle;
+	
 	if ( $angle=='150'){
 		//print "150";				
 		$cmd = "/usr/bin/python ./Servo_pos150.py 2>&1";
