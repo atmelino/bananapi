0296dfe034de83c38f3f3107967ba6b5881c4b53
jatnik <jatnik@jat>
2016-01-12 20:40:32 -0600

    new user interface

--- projects/develop/old/old.js
+++ projects/develop/old/old.js
@@ -9,3 +9,8 @@
 			//printlnMessage('messages', $("#frontLeft").spinner("value"));
 			//printlnMessage('messages', this);
 			//printlnMessage('messages', spinner.spinner("value"));
+			// 			ajax_setSpeed(ajaxCalled_setSpeed, "fl", 3);
+			// 			ajax_setSpeed(ajaxCalled_setSpeed, "fr", 3);
+			// 			ajax_setSpeed(ajaxCalled_setSpeed, "rl", 3);
+			// 			ajax_setSpeed(ajaxCalled_setSpeed, "rr", 3);
+
