abba7fcbea480e3b476fe76c5543307ed6f7128f
user <user@asu2>
2016-01-21 00:25:39 -0600

    web interface for power monitor

---
+++ projects/powerMonitor/ajaxFunctionsJSON.js
@@ -0,0 +1,40 @@
+
+function ajax_getValues(myParams) {
+	// printlnMessage('messages', "ajax_getValues() called");
+	try {
+		// Opera 8.0+, Firefox, Safari
+		ajaxGetValuesRequestJSON = new XMLHttpRequest();
+	} catch (e) {
+		// Internet Explorer Browsers
+		try {
+			ajaxGetValuesRequestJSON = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e) {
+			try {
+				ajaxGetValuesRequestJSON = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (e) {
+				// Something went wrong
+				alert("Your browser broke!");
+				return false;
+			}
+		}
+	}
+
+	ajaxGetValuesRequestJSON.onreadystatechange = ajaxCalled_getValues;
+	// printlnMessage('messages', JSON.stringify(myParams));
+	var requeststring;
+	requeststring = "getValuesJSON.php?json=" + JSON.stringify(myParams);
+	printlnMessage('messages', requeststring);
+	ajaxGetValuesRequestJSON.open("GET", encodeURI(requeststring), true);
+	ajaxGetValuesRequestJSON.send(null);
+
+}
+
+// Create a function that will receive data sent from the server
+function ajaxCalled_getValues() {
+	if (ajaxGetValuesRequestJSON.readyState == 4) {
+		//printlnMessage('messages', "ajaxCalled_getValues()");
+		getValuesAjaxJSON = ajaxGetValuesRequestJSON.responseText;
+		printlnMessage('messages', "response from PHP and python:");
+		printlnMessage('messages', getValuesAjaxJSON);
+	}
+}
---
+++ projects/powerMonitor/getValuesJSON.php
@@ -0,0 +1,34 @@
+<?php
+
+error_reporting(E_ALL);
+
+
+$json=$_GET['json'];
+echo "PHP: ".$json."\n";
+
+
+//$decoded = json_decode($_GET['json']);
+//echo $decoded;
+
+//$speedfl=$decoded->speedfl;
+//echo $speedfl;
+
+
+
+// $speed = $_GET['speed'];
+// $wheel = $_GET['wheel'];
+
+$cmd = "/usr/bin/python ./testSDL_Pi_INA3221.py ".escapeshellarg($json);
+
+$handle = popen($cmd, 'r');
+$res = fread($handle, 8192);
+pclose($handle);
+
+//var_dump($res);
+echo $res;
+
+echo 'end PHP program';
+
+
+
+?>
