00b91c03141c26ec2351e9d2e839d90c8151b715
user <user@asu2>
2016-01-25 22:43:05 -0600

    voltages, current global vars

--- projects/powerMonitor/getINA3221.py
+++ projects/powerMonitor/getINA3221.py
@@ -28,6 +28,18 @@ parsed_json = json.loads(myparams)
 if 'simulation' in parsed_json:
   simulation=parsed_json['simulation']     
 
+busvoltage1   = 0
+shuntvoltage1 = 0
+current_mA1   = 0
+loadvoltage1  = 0
+busvoltage2 = 0
+shuntvoltage2 = 0
+current_mA2 = 0
+loadvoltage2 = 0
+busvoltage3 = 0
+shuntvoltage3 = 0
+current_mA3 = 0
+loadvoltage3 = 0
 
 
 if simulation==1:
@@ -35,18 +47,15 @@ if simulation==1:
      shuntvoltage3 = 2
      current_mA3 = 8
      loadvoltage3 = 6
+     power3=loadvoltage3*current_mA3          
      
-     now=time.strftime("%Y-%m-%d %H:%M:%S")     
-     print "%12s %3.2f V  %3.2f mV  %3.2f V  %3.2f mA" % (now, busvoltage3, shuntvoltage3, loadvoltage3 ,current_mA3)
+     #now=time.strftime("%Y-%m-%d %H:%M:%S")     
+     #print "%12s %3.2f V  %3.2f mV  %3.2f V  %3.2f mA" % (now, busvoltage3, shuntvoltage3, loadvoltage3 ,current_mA3)
      #sys.stdout.write("%12s %3.2f V  %3.2f mV  %3.2f V  %3.2f mA" % (now, busvoltage3, shuntvoltage3, loadvoltage3 ,current_mA3))
 
 
 else:
      ina3221 = SDL_Pi_INA3221.SDL_Pi_INA3221(twi=2,addr=0x40)
-     shuntvoltage1 = 0
-     busvoltage1   = 0
-     current_mA1   = 0
-     loadvoltage1  = 0
      
      later =0
      if later>0:
@@ -63,10 +72,6 @@ else:
           print "LIPO_Battery Current 1:  %3.2f mA" % current_mA1
           print
           
-          shuntvoltage2 = 0
-          busvoltage2 = 0
-          current_mA2 = 0
-          loadvoltage2 = 0
           
           busvoltage2 = ina3221.getBusVoltage_V(SOLAR_CELL_CHANNEL)
           shuntvoltage2 = ina3221.getShuntVoltage_mV(SOLAR_CELL_CHANNEL)
@@ -79,18 +84,17 @@ else:
           print "Solar Cell Current 2:  %3.2f mA" % current_mA2
           print 
      
-     shuntvoltage3 = 0
-     busvoltage3 = 0
-     current_mA3 = 0
-     loadvoltage3 = 0
      
      busvoltage3 = ina3221.getBusVoltage_V(OUTPUT_CHANNEL)
      shuntvoltage3 = ina3221.getShuntVoltage_mV(OUTPUT_CHANNEL)
      current_mA3 = ina3221.getCurrent_mA(OUTPUT_CHANNEL)
      loadvoltage3 = busvoltage3 + (shuntvoltage3 / 1000)
      power3=loadvoltage3*current_mA3          
-     now=time.strftime("%Y-%m-%d %H:%M:%S")     
-     #print "%12s %3.2f V  %3.2f mV  %3.2f V  %3.2f mA " % (now, busvoltage3, shuntvoltage3, loadvoltage3 ,current_mA3)
-     print "%12s %3.2f V  %3.2f mV  %3.2f V  %3.2f mA %5.3f mW" % (now, busvoltage3, shuntvoltage3, loadvoltage3 ,current_mA3,power3)
-     #sys.stdout.write("%12s %3.2f V  %3.2f mV  %3.2f V  %3.2f mA" % (now, busvoltage3, shuntvoltage3, loadvoltage3 ,current_mA3))
+
+     
+     
+now=time.strftime("%Y-%m-%d %H:%M:%S")     
+print "%12s %3.2f V  %3.2f mV  %3.2f V  %3.2f mA %5.3f mW" % (now, busvoltage3, shuntvoltage3, loadvoltage3 ,current_mA3,power3)
+#print "%12s %3.2f V  %3.2f mV  %3.2f V  %3.2f mA " % (now, busvoltage3, shuntvoltage3, loadvoltage3 ,current_mA3)
+#sys.stdout.write("%12s %3.2f V  %3.2f mV  %3.2f V  %3.2f mA" % (now, busvoltage3, shuntvoltage3, loadvoltage3 ,current_mA3))
 
