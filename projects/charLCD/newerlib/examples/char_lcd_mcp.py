1f05c8d91f1784e89e2d7009d135518fa48f067c
bananapi <bananapi@myself>
2016-01-29 00:43:56 -0600

    busnumber 2

--- projects/charLCD/newerlib/examples/char_lcd_mcp.py
+++ projects/charLCD/newerlib/examples/char_lcd_mcp.py
@@ -2,7 +2,9 @@
 # Example using an RGB character LCD connected to an MCP23017 GPIO extender.
 import time
 
+print "char_lcd_mcp.py import CharLCD"
 import Adafruit_CharLCD as LCD
+print "char_lcd_mcp.py import MCP230xx"
 import Adafruit_GPIO.MCP230xx as MCP
 
 
@@ -26,6 +28,7 @@ lcd_rows    = 2
 # lcd_rows    = 4
 
 # Initialize MCP23017 device using its default 0x20 I2C address.
+print "char_lcd_mcp.py init gpio"
 gpio = MCP.MCP23017()
 
 # Alternatively you can initialize the MCP device on another I2C address or bus.
@@ -36,24 +39,27 @@ lcd = LCD.Adafruit_RGBCharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
 							lcd_columns, lcd_rows, lcd_red, lcd_green, lcd_blue, gpio=gpio)
 
 # Print a two line message
+print 'sending Hello world! to LCD'
 lcd.message('Hello\nworld!')
 
 # Wait 5 seconds
-time.sleep(5.0)
+time.sleep(2.0)
 
 # Demo showing the cursor.
+print 'Show cursor'
 lcd.clear()
 lcd.show_cursor(True)
 lcd.message('Show cursor')
 
-time.sleep(5.0)
+time.sleep(2.0)
 
 # Demo showing the blinking cursor.
+print 'Blink cursor'
 lcd.clear()
 lcd.blink(True)
 lcd.message('Blink cursor')
 
-time.sleep(5.0)
+time.sleep(2.0)
 
 # Stop blinking and showing cursor.
 lcd.show_cursor(False)
@@ -63,18 +69,22 @@ lcd.blink(False)
 lcd.clear()
 message = 'Scroll'
 lcd.message(message)
+print 'Scroll right'
 for i in range(lcd_columns-len(message)):
-	time.sleep(0.5)
+	time.sleep(0.3)
 	lcd.move_right()
+print 'Scroll left'
 for i in range(lcd_columns-len(message)):
-	time.sleep(0.5)
+	time.sleep(0.3)
 	lcd.move_left()
 
 # Demo turning backlight off and on.
+print 'Flash backlight'
 lcd.clear()
 lcd.message('Flash backlight\nin 5 seconds...')
-time.sleep(5.0)
+time.sleep(2.0)
 # Turn backlight off.
+print 'backlight off'
 lcd.set_backlight(0)
 time.sleep(2.0)
 # Change message.
