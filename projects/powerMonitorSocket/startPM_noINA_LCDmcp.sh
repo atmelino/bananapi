dc763d7e8248816a7d500e1b51343a471274a6ab
jatnik <jatnik@jat>
2016-02-08 19:50:06 -0600

    script for LCD mcp version

---
+++ projects/powerMonitorSocket/startPM_noINA_LCDmcp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+python powerMonitorSocket.py {\"simINA3221\":1\,\"LCD\":\"mcp\"}
+
+
+echo press enter
+
+read
+
--- projects/powerMonitorSocket/startPM_noINA_noLCD.sh
+++ projects/powerMonitorSocket/startPM_noINA_noLCD.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#python powerMonitorSocket.py {\"simINA3221\":1}
-
-#python powerMonitorSocket.py {\"simINA3221\":1\,\"LCD\":0}
 
 python powerMonitorSocket.py {\"simINA3221\":1\,\"LCD\":\"none\"}
 
