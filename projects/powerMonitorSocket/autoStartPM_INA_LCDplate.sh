51d2cb03f1e2aaba5d4ee711eff81e1d9bab9ef2
bananapi <bananapi@myself>
2016-02-09 23:44:07 -0600

    simulation off

--- projects/powerMonitorSocket/autoStartPM_INA_LCDplate.sh
+++ projects/powerMonitorSocket/autoStartPM_INA_LCDplate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sh -c 'cd /media/data/public_html/bananapi/projects/powerMonitorSocket; python powerMonitorSocket.py {\"simINA3221\":\,\"LCD\":\"plate\"}'
+sh -c 'cd /media/data/public_html/bananapi/projects/powerMonitorSocket; python powerMonitorSocket.py {\"simINA3221\":\0,\"LCD\":\"plate\"}'
 
 echo press enter
 
