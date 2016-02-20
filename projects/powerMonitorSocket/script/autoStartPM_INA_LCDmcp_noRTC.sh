d351fe278f80137fbcee65d65baacfade9d78839
bananapi <bananapi@myself>
2016-02-20 01:02:53 -0600

    non RTC version

---
+++ projects/powerMonitorSocket/script/autoStartPM_INA_LCDmcp_noRTC.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sh -c 'cd /media/data/public_html/bananapi/projects/powerMonitorSocket; python powerMonitorSocket.py {\"simINA3221\":0\,\"LCD\":\"mcp\"\,\"RTC\":0}'
+
+echo press enter
+
+read
+
