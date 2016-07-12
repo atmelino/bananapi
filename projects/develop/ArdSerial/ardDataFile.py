54a65d587c26629f70e78e927e3453fdf20f31e6
user <atmelino@github.com>
2016-07-11 23:53:43 -0500

    better keyboard quit

--- projects/develop/ArdSerial/ardDataFile.py
+++ projects/develop/ArdSerial/ardDataFile.py
@@ -1,20 +1,100 @@
-import serial
-from datetime import datetime
+#!/usr/bin/env python
+
+import os
 import time
+import sys
+import termios
+import atexit
+from select import select
+import datetime
+import serial
+
+
 
 ser = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
 
 
 
-try:
-    while 1:
-        response = ser.readline()
-        curdate = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        fh = open("ardData.txt", "a")
-        output="%s %s " %(curdate,response)
-        fh.write(output)
-        fh.close
-        print response
-except KeyboardInterrupt:
+class KBHit:
+
+    def __init__(self):
+        '''Creates a KBHit object that you can call to do various keyboard things.
+        '''
+
+
+        # Save the terminal settings
+        self.fd = sys.stdin.fileno()
+        self.new_term = termios.tcgetattr(self.fd)
+        self.old_term = termios.tcgetattr(self.fd)
+
+        # New terminal setting unbuffered
+        self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
+        termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
+
+        # Support normal-terminal reset at exit
+        atexit.register(self.set_normal_term)
+
+
+    def set_normal_term(self):
+        ''' Resets to normal terminal.  On Windows this is a no-op.
+        '''
+        termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
+
+
+    def getch(self):
+        ''' Returns a keyboard character after kbhit() has been called.
+            Should not be called in the same program as getarrow().
+        '''
+        s = ''
+        return sys.stdin.read(1)
+
+
+    def kbhit(self):
+        ''' Returns True if keyboard character was hit, False otherwise.
+        '''
+        if os.name == 'nt':
+            return msvcrt.kbhit()
+
+        else:
+            dr,dw,de = select([sys.stdin], [], [], 0)
+            return dr != []
+
+
+    def getArduinoData(self)	:
+        try:
+	        response = ser.readline()
+	        curdate = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	        fh = open("ardData.txt", "a")
+	        output="%s %s " %(curdate,response)
+	        fh.write(output)
+	        fh.close
+	        print response
+
+
+
+# Test    
+if __name__ == "__main__":
+
+    kb = KBHit()
+    i=1
+    print('Hit any key, or ESC to exit')
+
+    while True:
+        i=i+1
+        print (i)
+
+        kb.getINAValues()
+
+        if kb.kbhit():
+            c = kb.getch()
+            if ord(c) == 27: # ESC
+                break
+            print(c)
+        time.sleep(1.5)
+
+    # after break from while loop
+    kb.set_normal_term()
     ser.close()
+
+
 
